1b3c0b17a3559c271434b0accd1faa131a31a3e9
Kingshuk <sanyalbittu96@gmail.com>
2023-07-12 22:35:27 +0530

    my typescript project

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var charecter = "kingshuk sanyal    ";
+console.log(charecter);
+var input = document.querySelectorAll('input');
+console.log(input);
+input.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,10 @@
+const charecter = "kingshuk";
+console.log(charecter);
+
+const input = document.querySelectorAll('input');
+
+console.log(input);
+// comment
+input.forEach(input =>{
+    console.log(input);
+});
